613e58e06e7acaf1728b0d7dc812033cdf81e083
bhaskar4k <bhaskarndy@gmail.com>
2026-05-25 01:38:05 +0530

    multi db support and dal and common_models separate project

--- core-service/src/main/java/com/mycloud/core_service/CoreServiceApplication.java
+++ core-service/src/main/java/com/mycloud/core_service/CoreServiceApplication.java
@@ -2,12 +2,14 @@ package com.mycloud.core_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mycloud")
+@EntityScan(basePackages = "com.mycloud.common_models")
+@EnableJpaRepositories(basePackages = "com.mycloud.data_access_layer")
 public class CoreServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoreServiceApplication.class, args);
 	}
-
 }
