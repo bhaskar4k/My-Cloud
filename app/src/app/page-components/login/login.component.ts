fd73898767bf2dc9c8903bd6d729b9f9b9c93850
Bhaskar Nandy <bhaskarndy@gmail.com>
2026-05-30 17:43:19 +0530

    Register component's ui update

--- app/src/app/page-components/login/login.component.ts
+++ app/src/app/page-components/login/login.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
 
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]),
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm.value);
+  }
+
+  onReset(): void {
+    this.loginForm.reset();
+  }
 }
