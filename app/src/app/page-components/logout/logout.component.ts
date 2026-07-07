98be2047a6e5aa03b039a16128531b96622239d2
bhaskar4k <bhaskarndy@gmail.com>
2026-07-07 13:11:10 +0530

    Logout is done

--- app/src/app/page-components/logout/logout.component.ts
+++ app/src/app/page-components/logout/logout.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { CustomAlertComponent } from '../../common-components/custom-alert/custom-alert.component';
+import { ResponseTypeColor } from '../../constants/commonConsts';
 
 @Component({
   selector: 'app-logout',
@@ -7,5 +12,13 @@ import { Component } from '@angular/core';
   styleUrl: './logout.component.css'
 })
 export class LogoutComponent {
-
+  constructor(
+    private authService: AuthService,
+    private dialog: MatDialog,
+    private router: Router
+  ) {
+    this.dialog.open(CustomAlertComponent, { data: { text: "Logging out...", type: ResponseTypeColor.INFO } });
+    this.authService.Logout();
+    this.router.navigate(['/login']);
+  }
 }
